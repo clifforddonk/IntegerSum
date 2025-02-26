31b15ad2c4ac5566b62f5385e59e8247e7b83f07
clifforddonk <clifforddonk@gmail.com>
2025-02-26 09:57:43 +0000

    first commit

--- src/com/Clifford/Multiple.java
+++ src/com/Clifford/Multiple.java
@@ -2,11 +2,12 @@ package com.Clifford;
 
 public class Multiple {
     public static void generator() {
-        int x=0,sum=0;
-        while(x<8){
-            sum=0;
-            sum=x+sum;
+        int x=1,sum=0;
+        while(x<=8){
+            sum=+x;
             x++;
+
+
         }
         System.out.println("Sum of the first 8 integers is" +sum);
 
